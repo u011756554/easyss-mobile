bcf8be879c1dd2e2042ad830cd196272e4be34f5
AlbertLi <759330841@qq.com>
2018-03-23 09:17:41 +0800

    修改vpn的访问入口

--- platforms/android/app/src/main/java/com/weex/app/extend/EasyssModule.java
+++ platforms/android/app/src/main/java/com/weex/app/extend/EasyssModule.java
@@ -2,6 +2,7 @@ package com.weex.app.extend;
 
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.common.WXModule;
+import com.weex.app.WXPageActivity;
 
 import mobile.Mobile;
 
@@ -12,5 +13,20 @@ public class EasyssModule extends WXModule {
 		return  ret;
 	}
 
+    /**
+     * 开启vpn链接
+     * 注意：vpn发起请求，必须在WXPageActivity生命周期内调用。
+     */
+    public void start() {
+        if (WXPageActivity.instance != null) {
+            WXPageActivity.instance.startVpnService();
+        }
+    }
+
+    public void stop() {
+        if (WXPageActivity.instance != null) {
+            WXPageActivity.instance.stopVpnService();
+        }
+    }
 }
 
